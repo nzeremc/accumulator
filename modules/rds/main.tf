3d4cbaf656678c2c531f8d99a5a39c71cb898dea
Hetvi Shah <hetvi.sanghavi@ibm.com>
2026-03-05 17:40:38 +0530

    update

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -27,19 +27,19 @@ resource "aws_db_parameter_group" "postgres" {
   parameter {
     name         = "max_replication_slots"
     value        = "10"
-    apply_method = "pending-reboo"
+    apply_method = "pending-reboot"
   }
 
   parameter {
     name         = "max_wal_senders"
     value        = "10"
-    apply_method = "pending-reboo"
+    apply_method = "pending-reboot"
   }
 
   parameter {
     name         = "wal_sender_timeout"
     value        = "0"
-    apply_method = "pending-reboo"
+    apply_method = "pending-reboot"
   }
 
   tags = merge(
