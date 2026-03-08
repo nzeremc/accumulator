221b8da418054363637e6f15957621037d091184
Hetvi Shah <hetvi.sanghavi@ibm.com>
2026-03-08 14:53:21 +0530

    Update config

--- outputs.tf
+++ outputs.tf
@@ -12,17 +12,17 @@ output "ecr_db_init_repository_url" {
 # Networking Outputs
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = module.networking.vpc_id
+  value       = local.vpc_id
 }
 
 output "public_subnet_ids" {
   description = "IDs of public subnets"
-  value       = module.networking.public_subnet_ids
+  value       = local.public_subnet_ids
 }
 
 output "private_subnet_ids" {
   description = "IDs of private subnets"
-  value       = module.networking.private_subnet_ids
+  value       = local.private_subnet_ids
 }
 
 # ALB Outputs
